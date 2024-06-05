a1246adc914264370c59abe17f85b240e7114644
yryskeldi0101 <yryskeldi.9121@gmail.com>
2024-06-05 23:52:34 +0600

    return server api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,1 @@
-export const API = process.env.API_TEST || "http://127.0.0.1:8000"
-
-    // "https://alisher191.pythonanywhere.com"
+export const API = process.env.API_TEST || "https://alisher191.pythonanywhere.com"
